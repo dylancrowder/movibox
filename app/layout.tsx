0e2e45f5303cfe9fe0475d2b02dee2d54c64c64b
dylan <devdylancrowder@outlook.com>
2026-07-20 12:04:44 -0300

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -253,7 +253,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PSBNPHZX');
+              })(window,document,'script','dataLayer','GTM-T63FK29V');
             `,
           }}
         />
@@ -294,7 +294,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-PSBNPHZX"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-T63FK29V"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
